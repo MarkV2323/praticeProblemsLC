f8e2e5ae882e141cb4059e93454e220e9819e07e
MarkV2323 <41304553+MarkV2323@users.noreply.github.com>
2022-07-07 17:38:26 -0700

    added documentation for problem 4

--- algoStudyGuide/4squaresOfSortedArray.py
+++ algoStudyGuide/4squaresOfSortedArray.py
@@ -7,7 +7,21 @@ Given an integer array nums sorted in non-decreasing order,
 return an array of the squares of each number sorted in non-decreasing order.
 
 Problem Solution:
+Use two pointers, one from the start and one from the end, call these left and right pointers
 
+Compare nums elements at pointer values, nums has the following criteria
+1 - sorted in descending order (from least to greatest)
+2 - can contain negative numbers
+
+As a result, compare the absolute values of the input list's elements.
+
+if the left pointer value is less than or equal to the right pointer value,
+append the right value squared to the return list and move the right pointer by 1 element
+
+else append the left value squared to the return list and move the left pointer by 1 element
+
+By always appending the larger value, we are guaranteeing that our return list is in decreasing order.
+Before returning, we can just flip the order (now non-decreasing) to meet the requirement.
 """
 
 
