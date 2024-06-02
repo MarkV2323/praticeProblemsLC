2572bfa15aa0382910905f7a9dad2b59b2bdfcf2
Mark Vincent <41304553+MarkV2323@users.noreply.github.com>
2024-06-01 20:57:26 -0600

    Started WordPartten LC problem

---
+++ cppSolves/WordPattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Given a pattern and a string s, find if s follows the same pattern.
+ * 
+ * Here follow means a full match, such that there is a bijection between 
+ * a letter in pattern and a non-empty word in s.
+ *
+*/
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        
+    }
+};
+
+int main() {
+    cout << "Hello World!\n";
+}
